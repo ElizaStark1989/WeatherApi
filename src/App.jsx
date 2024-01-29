4272aa3ae220c926996fc344ce8e780fb2f9baea
Elizabeth <elizastark-1989@outlook.es>
2024-01-29 14:46:21 -0500

    termino con la base del proyecto y comienzo maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,53 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import WeatherCard from './components/WeatherCard';
 
 function App() {
-
   //const ApiKey = 'bb76c53d21aef6faf72b80d7d127166e';
 
   const [coords, setCoords] = useState();
   const [weather, setWeather] = useState();
-  
+  const [temp, setTemp] = useState();
 
-  const success = position=> {
-    //console.log(position);
+  const success = (position) => {
+    console.log(position);
     const obj = {
       lat: position.coords.latitude,
-      lon: position.coords.longitude,
+      lon: position.coords.longitude
     }
     setCoords(obj);
   }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  }, []);
 
   useEffect(() => {
     if (coords) {
       const ApiKey = 'bb76c53d21aef6faf72b80d7d127166e';
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${ApiKey}`;
       axios.get(url)
-        .then(res => setWeather(res.data))
+        .then(res => {
+          const obj = {
+            celsius: (res.data.main.temp - 273.15).toFixed(2),
+            fahrenheit: ((res.data.main.temp - 273.15) * (9/5) + 32).toFixed(2),
+          }
+          setTemp(obj);
+          setWeather(res.data);    
+        })
         .catch(err => console.log(err));
     }
   }, [coords]);
-  
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(success);
-  }, [])
-
-  console.log(coords);
-  console.log(weather);
-  
   return (
     <div>
-      <h1>Weather app</h1>
+      <WeatherCard 
+        weather = {weather}
+        temp =  {temp}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
