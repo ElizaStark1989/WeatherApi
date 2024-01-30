087032b7f748160fad3efab75d6952d0f1be6893
Elizabeth <elizastark-1989@outlook.es>
2024-01-30 03:21:57 -0500

    Se terminan los ultimos ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import WeatherCard from './components/WeatherCard';
+const ApiKey = 'bb76c53d21aef6faf72b80d7d127166e';
 
 function App() {
   //const ApiKey = 'bb76c53d21aef6faf72b80d7d127166e';
@@ -9,6 +10,10 @@ function App() {
   const [coords, setCoords] = useState();
   const [weather, setWeather] = useState();
   const [temp, setTemp] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [textInput, setTextInput] = useState('');
+  const [finder, setFinder] = useState();
+  const [hasError, setHasError] = useState(false);
 
   const success = (position) => {
     console.log(position);
@@ -25,7 +30,6 @@ function App() {
 
   useEffect(() => {
     if (coords) {
-      const ApiKey = 'bb76c53d21aef6faf72b80d7d127166e';
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${ApiKey}`;
       axios.get(url)
         .then(res => {
@@ -36,16 +40,58 @@ function App() {
           setTemp(obj);
           setWeather(res.data);    
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+          setHasError(true);
+          console.log(err);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        }, );
     }
   }, [coords]);
 
+  useEffect(() => {
+    if(textInput){
+      const url= `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${ApiKey}`;
+      axios.get(url)
+        .then(res => {
+          const obj = {
+            celsius: (res.data.main.temp - 273.15).toFixed(2),
+            fahrenheit: ((res.data.main.temp - 273.15) * (9/5) + 32).toFixed(2),
+          }
+          setTemp(obj);
+          setHasError(false);
+          setFinder(res.data)
+        })
+        .catch(err => {
+          setHasError(true);
+          console.log(err);
+        });
+    }
+  }, [textInput]);
+  console.log(textInput);
+
   return (
     <div className='app'>
-      <WeatherCard 
-        weather = {weather}
-        temp =  {temp}
-      />
+      {
+        isLoading?
+         <h2>Loading...</h2>
+         :
+         textInput?
+         <WeatherCard 
+          weather = {finder}
+          temp =  {temp} 
+          setTextInput = {setTextInput} 
+          hasError = {hasError}
+        />
+        :
+        <WeatherCard 
+          weather = {weather}
+          temp =  {temp} 
+          setTextInput = {setTextInput} 
+          hasError = {hasError}
+        />
+      }
     </div>
   );
 }
